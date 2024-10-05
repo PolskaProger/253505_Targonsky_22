c94af748e3f0361ffb05b68aabbfb44e9b684aa5
polskaproger <dimatarg0110@gmail.com>
2024-10-05 13:04:16 +0300

    strweb-1

---
+++ STRWEB/LR1/CosmetologyCenter/doctors/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
+
+    first_name = models.CharField(max_length=20, help_text="Enter type")
+    last_name = models.CharField(max_length=20, help_text="Enter type")
+
+    category = models.ManyToManyField('DoctorCategories', help_text="Choose doctor category")
+
+    work_experience = models.IntegerField(help_text="Enter work experience")
+
+    phone = models.CharField(max_length=20, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    image = models.ImageField(upload_to='images', blank=True, null=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+
+    def __str__(self):
+        return f"{self.last_name} {self.first_name}"
+
+
+class DoctorCategories(models.Model):
+    designation = models.CharField(max_length=20, help_text="Enter type")
+
+    def __str__(self):
+        return self.designation
